7619d0bcaab822120f2b70632d147d2592930ca3
Tu Phan <tusphananh@gmail.com>
2023-02-09 21:13:40 +0700

    add ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { CaretRightOutlined, CopyOutlined } from "@ant-design/icons";
+import { Button, Card, Input, message, Space } from "antd";
+import { useState } from "react";
+import "./App.css";
+
+const { TextArea } = Input;
 
 function App() {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(output);
+    messageApi.open({
+      type: "success",
+      content: "Copied to clipboard !",
+    });
+  };
+
+  const onRun = () => {
+    try {
+      messageApi.open({
+        type: "success",
+        content: "Completed !",
+      });
+    } catch (error) {
+      messageApi.open({
+        type: "fail",
+        content: `${error}`,
+      });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {contextHolder}
+      <Card className="card" title="Calculation Tool" bordered={false}>
+        <Space direction="vertical" size="middle" style={{ width: "100%" }}>
+          <TextArea rows={4} placeholder="Input Data" onChange={setInput} />
+          <TextArea rows={4} placeholder="Output Data" disabled />
+          <Space>
+            <Button
+              type="primary"
+              onClick={onRun}
+              icon={<CaretRightOutlined />}
+              disabled={!input}
+            >
+              Run
+            </Button>
+            <Button
+              type="primary"
+              shape="round"
+              icon={<CopyOutlined />}
+              onClick={copyToClipboard}
+              disabled={!output}
+            />
+          </Space>
+        </Space>
+      </Card>
     </div>
   );
 }
